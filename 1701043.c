cffab0b0d7fc69fe767433ae88462d2fa903a3ba
Loay Elshall <Loay.elshall@icloud.com>
2021-12-14 20:27:43 +0200

    Initial Commit

---
+++ 1701043.c
@@ -0,0 +1,80 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <time.h>
+
+long double factorial(long double n) {
+    long double result = 1;  
+    for (long double i = 2; i < n; i++)
+    {
+        result *= i + 1;
+    }
+    return result;
+}
+long double power(long double base, int exp) {
+    long double result = 1;
+    for (int i = 1; i <= exp; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    int upperLimit;
+    double x;
+    int rank, size;
+    long double cosX = 0.0; 
+    long double time1, time2;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    setbuf(stdout, NULL);
+
+    if (rank == 0) {
+        printf("Enter the upper limit: ");
+        scanf("%d", &upperLimit);
+        printf("Enter the value of x: ");
+        scanf("%lf", &x);
+    }
+    MPI_Bcast(&upperLimit, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&x, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    time1 = MPI_Wtime();
+    for (long long i = rank; i <= upperLimit; i += size) {       
+        cosX += (long double)((power(-1, i) * power(x, 2*i)) / (long double)(factorial(2 * i)));        
+    }
+    time2 = MPI_Wtime();
+
+    long double globalCosX = 0.0;
+    MPI_Reduce(&cosX, &globalCosX, 1, MPI_LONG_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("\n");
+        printf("The cosine of %lf is %Lf\n", x, globalCosX);
+        printf("The time is %Lf Seconds\n", time2 - time1);
+        printf("\n");
+    }
+
+    MPI_Finalize();
+    
+    // Sequential code
+    if (rank == 0) {
+        long double cosX_seq = 0.0;
+        clock_t t;
+        t = clock();
+        for (int i = 0; i <= upperLimit; i++)
+        {
+            cosX_seq += (long double)((power(-1, i) * power(x, 2*i)) / (long double)(factorial(2 * i)));
+        }
+        t = clock() - t;
+        
+        printf("\n");
+        printf("The sequential cosine of %lf is %Lf\n", x, cosX_seq);
+        printf("The time is %lf Seconds\n", ((double)t)/CLOCKS_PER_SEC);
+    }  
+}
+
+
+
+    
---
+++ README.txt
@@ -0,0 +1,6 @@
+1) Open Terminal and navigate to the directory where 1701043.c exists
+2) type mpicc -O -o 1701043.out 1701043.c
+3) type mpirun -n 6 1701043.out (where 6 is the number of processing units)
+4) Enter the upper value of i
+5) Enter the value of X
+6) The Results with the time for each operation will be printed
